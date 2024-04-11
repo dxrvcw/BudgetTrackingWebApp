8f0d57c3de5376ff53b8447add17b90170d1008f
dxrvcw <oleksandruhnotk5@gmail.com>
2024-04-11 15:11:58 +0300

    Added frontend

---
+++ login.js
@@ -0,0 +1,31 @@
+let emailLabel = document.getElementById("email");
+let passwordLabel = document.getElementById("password");
+let submitButton = document.getElementById("submit");
+
+if (localStorage.getItem("userId")) {
+	window.location.href = "app.html";
+}
+
+submitButton.addEventListener("click", login);
+
+function login() {
+	let email = emailLabel.value;
+	let password = passwordLabel.value;
+
+	fetch(`http://localhost:8080/login?login=${email}&password=${password}`).then(
+		(response) => {
+			if (response.status === 401) {
+				alert("Wrong email or password");
+			} else if (response.status !== 200) {
+				alert("Error: " + response.status);
+			} else {
+				window.location.href = "app.html";
+
+				response.json().then((data) => {
+					localStorage.setItem("userId", data.userId);
+					localStorage.setItem("email", data.email);
+				});
+			}
+		}
+	);
+}
---
+++ register.js
@@ -0,0 +1,40 @@
+let emailLabel = document.getElementById("email");
+let passwordLabel = document.getElementById("password");
+let repeatPasswordLabel = document.getElementById("repeatPassword");
+let submitButton = document.getElementById("submit");
+
+submitButton.addEventListener("click", register);
+
+function register() {
+	let email = emailLabel.value;
+	let password = passwordLabel.value;
+	let repeatPassword = repeatPasswordLabel.value;
+
+	if (validateEmail(email) && validatePassword(password, repeatPassword)) {
+		fetch(
+			`http://localhost:8080/register?login=${email}&password=${password}`
+		).then((response) => {
+			if (response.status !== 200) {
+				alert("Error: " + response.status);
+			} else {
+				window.location.href = "login.html";
+			}
+		});
+	}
+}
+
+function validateEmail(email) {
+	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+		return true;
+	}
+	alert("You have entered an invalid email address!");
+	return false;
+}
+
+function validatePassword(password, secondPassword) {
+	if (password === secondPassword && password.length >= 8) {
+		return true;
+	}
+	alert("Passwords do not match or are too short!");
+	return false;
+}
